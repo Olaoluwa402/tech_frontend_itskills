193561b5433bfe82f0dd29d594150c8c885e9295
olaoluwa402 <ibukundaniel402@yahoo.co.uk>
2023-04-12 13:32:25 +0100

    regular expressionn

---
+++ Javascript_basics/js/regex.js
@@ -0,0 +1,52 @@
+//regular expression - searchs for pattern in a test
+//Two ways to declare a regex instance
+//1. using RegExp costructor class
+const { log } = console;
+const pattern_1 = "spam";
+const reg = new RegExp(pattern_1, "ig");
+log(reg, "reg");
+
+//2. using two forward sashes //
+const reg_2 = /spam/gi;
+
+const data_1 =
+  "Javascript is a programming language with powerful capabilities both o the web, mobile and desktop. Javascript is at per with Python";
+
+//test that the data above contains javascript. ignore case
+const isPresent = /javascript/gi.test(data_1); //usig test method
+log(isPresent, "isPreset");
+
+//find all all matchs of the supplied input field
+const subitBtn = document.getElementById("btn-submit");
+subitBtn.addEventListener("click", function (e) {
+  const inputElement = document.getElementById("name");
+  const inputValue = inputElement.value;
+  log(inputValue);
+
+  const pattern_2 = new RegExp(inputValue, "ig");
+  const searchResult = data_1.match(pattern_2);
+  const ul = document.getElementById("list");
+
+  if (searchResult) {
+    searchResult.forEach((element) => {
+      log(element, "serach");
+      const li = document.createElement("li");
+      li.className = "item";
+      li.style.color = "green";
+      li.textContent = element;
+      ul.appendChild(li);
+    });
+  } else {
+    const li = document.createElement("li");
+    li.textContent = "No item found";
+    ul.appendChild(li);
+  }
+});
+
+//search - returns an index if preset ad -1 if absent
+const search = data_1.search(/javacript/gi);
+log(search, "search");
+
+//replace every javascript foud
+const replace = data_1.replace(/javascript/gi, "Python");
+log(replace, "replace");
--- Javascript_basics/js/set.js
+++ Javascript_basics/js/set.js
@@ -30,3 +30,62 @@ log(fruitCap, "fruitCap");
 //delete
 companies.delete("google");
 log(companies);
+
+//has
+const has = companies.has("Microsoft");
+log(has, "has");
+
+//clear
+companies.clear();
+
+log(companies);
+
+//union of set
+const arr_1 = [1, 2, 3, 4, 5];
+const arr_2 = [3, 4, 7, 8, 7];
+const arr_3 = [...arr_1, ...arr_2];
+
+const unionSet = new Set(arr_3);
+log(unionSet, "unionSet");
+
+//intersection of set
+const B = new Set(arr_2);
+[3, 4, 7, 8];
+const intersectArr = arr_1.filter((number) => B.has(number)); //[3,4]
+const interSect = new Set(intersectArr);
+log(interSect, "intersect");
+
+//difference
+const differenceArr = arr_1.filter((number) => !B.has(number)); //[3,4]
+const difference = new Set(differenceArr);
+log(difference, "difference");
+
+//Map
+
+//create map from array
+const mapArr = [
+  ["Oyo", "Ibadan"],
+  ["Kogi", "Lokoja"],
+  ["Lagos", "Ikeja"],
+];
+
+const newMap = new Map(mapArr);
+log(newMap, "newMap");
+
+//add items to an empty map
+const addMap = new Map();
+addMap.set("Oyo", "Ibadan");
+addMap.set("Ekiti", "Ado");
+
+//get item of a map collection
+const getItem = newMap.get("Oyo"); // Ibadan
+log(getItem, "getItem");
+
+//iterate over  a map collection
+for (let item of newMap) {
+  log(item);
+}
+
+for (let [state, capital] of newMap) {
+  log(state, capital);
+}
